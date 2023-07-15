b17ea4ad73fa5feb2ec47987bddd000701bb405a
Nguyen Tommy <nttltommy2000@gmail.com>
2023-07-15 19:00:05 +0700

    15072023

--- src/App.jsx
+++ src/App.jsx
@@ -5,12 +5,13 @@ import L_body1 from "./components/L_body1/L_body1";
 import L_body2 from "./components/Lbody2/L_body2";
 import L_body23 from "./components/L_body23/L_body23";
 import L_body11 from "./components/L_body1.1/L_body11";
-
+import Home from "./pages/Home";
 const App = () => {
   const router = createBrowserRouter([
     {
       path: "/",
-      element: <L_body />,
+      element: <Home />,
+      
     },
   ]);
 
---
+++ src/pages/Home.jsx
@@ -0,0 +1,11 @@
+import UseCase from "./UseCase";
+import L_body from "../components/L_body/L_body";
+const Home = () => {
+  return (
+    <>
+      <L_body />
+      <UseCase />
+    </>
+  );
+};
+export default Home;
